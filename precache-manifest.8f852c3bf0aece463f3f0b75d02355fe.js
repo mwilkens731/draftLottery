a9f1de7c4064080e48cdd41f641373ce833619b8
mwilkens731 <matthew.j.wilkens@gmail.com>
2019-08-12 21:26:18 -0500

    Updates

---
+++ precache-manifest.8f852c3bf0aece463f3f0b75d02355fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a0fba98acefbfd3369",
+    "url": "/draftLottery/static/js/runtime~main.b182df57.js"
+  },
+  {
+    "revision": "c58090fc3d1271a8d492",
+    "url": "/draftLottery/static/js/main.b43ca2a6.chunk.js"
+  },
+  {
+    "revision": "8271ea714710e72f8db1",
+    "url": "/draftLottery/static/js/2.7ac7a8c5.chunk.js"
+  },
+  {
+    "revision": "c58090fc3d1271a8d492",
+    "url": "/draftLottery/static/css/main.0f864bf1.chunk.css"
+  },
+  {
+    "revision": "8271ea714710e72f8db1",
+    "url": "/draftLottery/static/css/2.bed5c86d.chunk.css"
+  },
+  {
+    "revision": "bb1cbcc1ca30b7eb06186641eb5d2006",
+    "url": "/draftLottery/index.html"
+  }
+];
